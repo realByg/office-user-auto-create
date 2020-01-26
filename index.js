c044c54e6b769d1d38fbfe6e696ebcfa8cc62c33
Byg <another4erik@gmail.com>
2020-01-26 21:40:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ var html = `
 
 <body>
 
-    <a href="https://github.com/you"><img width="149" height="149" src="https://github.blog/wp-content/uploads/2008/12/forkme_right_darkblue_121621.png?resize=149%2C149" class="attachment-full size-full" alt="Fork me on GitHub" data-recalc-dims="1" style="position: absolute; top: 0;z-index: 999999;right: 0"></a>
+    <a href="https://github.com/zayabighead/msautocreate"><img width="149" height="149" src="https://github.blog/wp-content/uploads/2008/12/forkme_right_darkblue_121621.png?resize=149%2C149" class="attachment-full size-full" alt="Fork me on GitHub" data-recalc-dims="1" style="position: absolute; top: 0;z-index: 999999;right: 0"></a>
 
     <div id="fullpage">
 
