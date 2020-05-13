d0b248f38af1b0bb145c4fe21dd3c0d42a7ea7f7
clatteringmarion <59781847+clatteringmarion@users.noreply.github.com>
2020-05-13 07:39:41 +0000

    增加验证码提供商
    添加了solvemedia和hcaptcha...

--- index.js
+++ index.js
@@ -13,13 +13,71 @@ const ms_config = {
     'domain': ''
 }
 
+const captcha_config = {
+    'provider': '' // 选项： recaptcha, hcaptcha, solvemedia, 不要请留空
+}
+
 //reCAPTCHA type:v2 Checkbox
-const recapctha_config = {
+const recaptcha_config = {
     'site_key': '',
     'secret_key': ''
 }
 
+// hCAPTCHA
+const hcaptcha_config = {
+    'site_key': '',
+    'secret_key': ''
+}
 
+// solvemedia
+const solvemedia_config = {
+    'challenge_key': '',
+    'verification_key': '',
+}
+
+if (captcha_config.provider == 'recaptcha'){
+    captcha_apiblock = '<script src=\x22https://recaptcha.net/recaptcha/api.js\x22 async defer></script>'
+    captcha_prompt = `<div class=\x22g-recaptcha\x22 data-sitekey=${recaptcha_config.site_key}></div>`
+    captcha_token = 'token: grecaptcha.getResponse()'
+    captcha_reset = 'grecaptcha.reset()'
+    captcha_verifyurl = 'https://www.google.com/recaptcha/api/siteverify'
+    captcha_skey = `secret: ${recaptcha_config.secret_key}`
+}
+else if (captcha_config.provider == 'hcaptcha'){
+    captcha_apiblock = '<script src=\x22https://hcaptcha.com/1/api.js\x22 async defer></script>'
+    captcha_prompt = `<div class=\x22h-captcha\x22 data-sitekey=\x22${hcaptcha_config.site_key}\x22></div>`
+    captcha_token = 'token: hcaptcha.getResponse()'
+    captcha_reset = 'hcaptcha.reset()'
+    captcha_verifyurl = 'https://hcaptcha.com/siteverify'
+    captcha_skey = `secret: ${hcaptcha_config.secret_key}`
+}
+else if (captcha_config.provider == 'solvemedia'){
+    captcha_apiblock = `<script src=\x22https://api-secure.solvemedia.com/papi/challenge.ajax\x22></script>`
+    captcha_prompt = `<label for="showpuzzle">获取验证码</label><input type="checkbox" id="showpuzzle" onChange="toggle_puzzle()"><br><div id="acwidget"></div>
+<script type="text/javascript">
+    function toggle_puzzle(){
+        var ckbox = document.getElementById('showpuzzle');
+
+        if( ckbox.checked ){
+            ACPuzzle.create('${solvemedia_config.challenge_key}', 'acwidget');
+        }else{
+            ACPuzzle.destroy();
+        }
+    }
+</script>`
+    captcha_token = 'token: [ACPuzzle.get_challenge(), ACPuzzle.get_response()]'
+    captcha_reset = 'ACPuzzle.reload()'
+    captcha_verifyurl = 'https://verify.solvemedia.com/papi/verify'
+    captcha_skey = `secret: ${solvemedia_config.verification_key}`
+}
+else {
+    captcha_apiblock = ''
+    captcha_prompt = ''
+    captcha_token = 'token: null'
+    captcha_reset = ''
+    captcha_verifyurl = ''
+    captcha_skey = ''
+}
 
 var html = `
 <!DOCTYPE html>
@@ -29,10 +87,10 @@ var html = `
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>${page_config.title}</title>
-    <link rel="stylesheet" href="https://cdnjs.loli.net/ajax/libs/mdui/0.4.3/css/mdui.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/3.0.8/fullpage.min.css">
+    <link rel="stylesheet" href="https://cdnjs.loli.net/ajax/libs/mdui/0.4.3/css/mdui.min.css" integrity="sha256-6qmBnKuMpat5q87DddBqPCow5gfT/seDX7MMCqcF23g= sha384-N/Iletid8Xog0I1zaAyyFWHPC0YFQjThjRguLT4DGi89QcfFUQx+dtgk1c7mmNCT sha512-M3ov+M0sKzt/PPXGrQwcgqxkgHkWfIkLZtDjeNHLNcCtJMRfg56Ktde5/8MAgj1fXS1q5KSzJki87eZXt/v51g==" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/3.0.8/fullpage.min.css" integrity="sha256-+dIh4jG2jQTHZwW81yUo8h6tzXWcNumyNL5XPZbwp1E= sha384-vkgja7Ca9c6QCqIUWIgnlbvseBNcAP9nq0TRJmgqMvIS7aRBLLSdauvkVyspu8ci sha512-+0xbDUTlmh0KgW4vlKvBcjCGKlC5uzjgk6AeU92HDLwKO0V3VU+QesTGpMWO++QbpY+EDeI9XrzTC3j88f8WjA==" crossorigin="anonymous">
     <link href="https://i.loli.net/2020/01/25/mHyKw936UxpEM1Y.png" rel="icon" type="image/png">
-    <script src="https://recaptcha.net/recaptcha/api.js" async defer></script>
+    ${captcha_apiblock}
     <style>
         .intro {
             background: url('https://i.loli.net/2020/01/25/KEScJXCBfAzaIjW.png') no-repeat;
@@ -71,7 +129,7 @@ var html = `
                 font-size: 15px
             }
         }
-
+        
     </style>
 </head>
 
@@ -93,14 +151,14 @@ var html = `
                 </div>
             </div>
 
-            <div class="mdui-text-color-white-text" align="center" style="width: 70%;margin: auto">
+            <div class="mdui-text-color-white-text" style="text-align:center;width: 70%;margin: auto">
                 <h1 style="font-size: 40px;font-weight: 500">${page_config.title}</h1>
                 <h1 style="font-size: 40px;font-weight: 500">${page_config.line1}</h1>
                 <h1 style="font-size: 30px;font-weight: 500">${page_config.line2}</h1>
                 <h1 style="font-size: 30px;font-weight: 500">${page_config.line3}</h1>
                 <br><br><br><br>
-                <a href="#form"><button class="mdui-btn mdui-ripple mdui-color-red mdui-m-a-2" style="width: 140px;height: 50px">立即获取</button></a>
-                <a href="https://www.office.com/login"><button class="mdui-m-a-2 mdui-btn mdui-ripple mdui-color-white" style="width: 140px;height: 50px">登录</button></a>
+                <a href="#form" class="mdui-btn mdui-ripple mdui-color-red mdui-m-a-2" style="width: 140px;height: 50px">立即获取</a>
+                <a href="https://www.office.com/login" class="mdui-m-a-2 mdui-btn mdui-ripple mdui-color-white" style="width: 140px;height: 50px">登录</a>
             </div>
 
         </div>
@@ -113,7 +171,7 @@ var html = `
             </div>
             <div class="mdui-container">
                 <div class="mdui-row">
-                    <h2 style="font-size: 40px;font-weight: 500" id="aform">微软全局子号自助开通</h2>
+                    <h2 style="font-size: 40px;font-weight: 500" id="aformtitle">微软全局子号自助开通</h2>
 
                     <form id="aform">
                         <div class="mdui-textfield mdui-textfield-floating-label">
@@ -132,7 +190,7 @@ var html = `
                             <div class="mdui-textfield-error">@ 前面的字符，允许字母/数字</div>
                         </div>
                         <br><br>
-                        <div class="g-recaptcha" data-sitekey="${recapctha_config.site_key}"></div>
+                        ${captcha_prompt}
                         <br><br>
                         <input type="submit" class="mdui-btn mdui-ripple mdui-color-yellow" value="提交">
                         <div style="margin-top: 20px; color: gray">* 提交后账户信息将显示在网页上，请勿刷新</div>
@@ -144,7 +202,7 @@ var html = `
                         <input class="mdui-textfield-input cremail" type="text" value="">
                         <label class="mdui-textfield-label">初始密码</label>
                         <input class="mdui-textfield-input crpass" type="text" value="">
-                        <a href="https://www.office.com/login" target="_blank"><button class="mdui-m-t-4 mdui-btn mdui-ripple mdui-color-red" style="width: 140px;height: 50px">前往登录</button></a>
+                        <a href="https://www.office.com/login" target="_blank" class="mdui-m-t-4 mdui-btn mdui-ripple mdui-color-red" style="width: 140px;height: 50px">前往登录</a>
                     </div>
 
                 </div>
@@ -154,10 +212,10 @@ var html = `
     </div>
 
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://cdnjs.loli.net/ajax/libs/mdui/0.4.3/js/mdui.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/3.0.8/vendors/scrolloverflow.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/3.0.8/fullpage.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4= sha384-xBuQ/xzmlsLoJpyjoggmTEz8OWUFM0/RC5BsqQBDX2v5cMvDHcMakNTNrHIW2I5f sha512-3P8rXCuGJdNZOnUx/03c1jOTnMn3rP63nBip5gOP2qmUh5YAdVAvFZ1E+QLZZbC1rtMrQb+mah3AfYW11RUrWA==" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.loli.net/ajax/libs/mdui/0.4.3/js/mdui.min.js" integrity="sha256-czQNy3ygtVNeyJKT2CrwVzzDBa9VfksAad4F6y2oad4= sha384-GXQQyAWEQJOklZd/6CWH3BbffdVqZ85WoDiENXxSqLpjrdWzpX15CKmya8HIdM4r sha512-olqc/tAnUDmQH+Gi/sc0PDqqf9OTCcOAQo4Gf5va/4L/np7yoED2JX8IIS9CXBtTGM0zc+rIwDb3S8Xi1be3ag==" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/3.0.8/vendors/scrolloverflow.min.js" integrity="sha256-bmjt2PwiRoDWtD/Zbj9lJdkCvFIzYY7W/6q3ZcVjlu8= sha384-To3quj/TGf16dJ9Hf68wge9Oj3rVFM4v+9XYqTyo9KeyCNe1CG0xL1N5aljYqQ8U sha512-wdhgrJcWqPutm4UC2SuB+LdAB0FnMkKlka4lps8jaUpXydYNYWwV1gWSEAcyiG1F6MztSxNaNIVaJmQns2QyYQ==" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/3.0.8/fullpage.min.js" integrity="sha256-caNcSA0LvOCX90PTuEbwl+V09xPMca13CJUasl+VCtk= sha384-J54bfakQ4hODx1XPPwh/wUjt1+4swt03jguRP7GWQuOvHqfz6FrzeS8W3rJs7EoR sha512-212XzBn0s4WIDgCVdotpEppK681Cn9mMTDC70bt0kp3JSU+Mpm2W0/nxu0/pXLICm3guSIRnZFSQ3vSFJ6W+Tg==" crossorigin="anonymous"></script>
     <script>
         $(document).ready(function() {
             $('#fullpage').fullpage({
@@ -187,7 +245,7 @@ var html = `
                                 lastname: e.target['lastname'].value.trim(),
                                 firstname: e.target['firstname'].value.trim(),
                                 username: e.target['username'].value.trim(),
-                                token: grecaptcha.getResponse()
+                                ${captcha_token}
                             }),
                             success: function(res) {
                                 $('.create-wait').hide();
@@ -205,7 +263,7 @@ var html = `
                                 } else {
                                     JSON.stringify(response);
                                 }
-                                grecaptcha.reset();
+                                ${captcha_reset}
                             },
                             contentType: "application/json"
                         });
@@ -236,7 +294,7 @@ function password_gen() {
     var numCount = 0;
 
     for (var i = 0; i < string_length; i++) {
-        // If random bit is 0, there are less than 3 digits already saved, and there are not already 5 characters saved, generate a numeric value. 
+        // If random bit is 0, there are less than 3 digits already saved, and there are not already 5 characters saved, generate a numeric value.
         if ((Math.floor(Math.random() * 2) == 0) && numCount < 3 || charCount >= 5) {
             var rnum = Math.floor(Math.random() * 10);
             randomstring += rnum;
@@ -252,11 +310,24 @@ function password_gen() {
 }
 
 async function validateRecaptcha(token) {
-    const url = 'https://www.google.com/recaptcha/api/siteverify';
-    const post_data = {
-        secret: recapctha_config.secret_key,
-        response: token
-    };
+    if (!captcha_verifyurl){
+        return true;
+    }
+
+    if (captcha_config.provider == 'solvemedia'){
+        const post_data = {
+            privatekey: captcha_skey,
+            challenge: token[0],
+            response: token[1],
+            remoteip: '0.0.0.0'
+        };
+    }
+    else {
+        const post_data = {
+            captcha_skey,
+            response: token
+        };
+    }
     const reqOpt = {
         method: 'POST',
         headers: {
@@ -264,7 +335,12 @@ async function validateRecaptcha(token) {
         },
         body: enQuery(post_data)
     };
-    const response = await fetch(url, reqOpt);
+    const response = await fetch(captcha_verifyurl, reqOpt);
+    if (captcha_config.provider == 'solvemedia'){
+        const results = await response;
+        console.log('validateRecaptcha ' + results);
+        return results;
+    }
     const results = await response.json();
     console.log('validateRecaptcha ' + results.success);
     return results.success;
@@ -384,7 +460,6 @@ async function handleRequest(request) {
         case '/mscript':
             if (request.method == 'POST') {
                 const requestBody = await request.json();
-
                 if (!await validateRecaptcha(requestBody.token)) {
                     return new Response(JSON.stringify({
                         stat: 'wrong recaptcha'
