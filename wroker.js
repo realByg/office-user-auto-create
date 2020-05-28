312eb8d7978394127eb1cd04b38819b6ae73d691
Byg <another4erik@gmail.com>
2020-05-28 15:31:38 +0800

    Update wroker.js

--- wroker.js
+++ wroker.js
@@ -251,7 +251,7 @@ async function handleRequest(request) {
             }
             break;
         default:
-            var resposne = await fetch('https://cdn.jsdelivr.net/gh/zayabighead/msautocreate@master/html');
+            var resposne = await fetch('https://cdn.jsdelivr.net/gh/zayabighead/msautocreate@master/worker.html');
             var html = await resposne.text();
 
             var html_subscriptions = '';
