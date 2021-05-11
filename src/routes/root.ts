733cecc6dc9104be692e573f22ea2f466b7bfff9
ZayaByghead <ZayaByghead@gmail.com>
2021-05-11 14:59:24 +0800

    root response

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -1,7 +1,10 @@
 import {Middleware} from '@cfworker/web'
 
+const asset = 'https://cdn.jsdelivr.net/gh/zayabighead/office-user-auto-create@client/bundle/index.html'
+
 const root: Middleware = async ({req, res}) => {
-    res.body = 'it works'
+    const response = await fetch(asset)
+    res.body = response.text()
 }
 
 export default root
