ae89b2c62402c6941665697d6a4c95f85d71dc4f
Byg <another4erik@gmail.com>
2021-05-12 14:39:06 +0800

    Update root.ts

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -1,6 +1,6 @@
 import {Middleware} from '@cfworker/web'
 
-const asset = 'https://cdn.jsdelivr.net/gh/zayabighead/office-user-auto-create@client/bundle/index.html'
+const asset = 'https://cdn.jsdelivr.net/gh/zayabighead/office-user-auto-create@static/bundle/index.html'
 
 const root: Middleware = async ({req, res}) => {
     const response = await fetch(asset)
